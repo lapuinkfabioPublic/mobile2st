50e9dd4ddc2dd4687c524da9641c10b2c539e94a
Fabio Leandro Lapuinka <lapuinkafabio@gmail.com>
2026-01-30 14:48:07 -0300

    day

--- app/src/main/java/com/fll/myapplication/LoginActivity.kt
+++ app/src/main/java/com/fll/myapplication/LoginActivity.kt
@@ -1,14 +1,17 @@
 package com.fll.myapplication
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.fll.myapplication.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener{
 
     private lateinit var binding: ActivityLoginBinding
 
@@ -20,11 +23,37 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        binding.btnLogin.text = "Click here to login"
+        //se aprendemos com tentativas e acertos, não aprenderemos nada com A.I
+
+        binding.btnLogin.setOnClickListener ( this )
+        binding.btnCadastro.setOnClickListener ( this )
     }
+
+    //tratando ev
+    override fun onClick(v: View?) {
+
+        if (v?.id == R.id.btnLogin)
+        {
+            val email = binding.editMail.text.toString()
+            val password = binding.editSenha.text.toString()
+
+            if(!email.isNullOrEmpty() && !password.isNullOrEmpty())
+            {
+                val intent :  Intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this, R.string.toast_msg_informe_dados, Toast.LENGTH_SHORT).show()
+            }
+        }
+        else if (v?.id == R.id.btnCadastro){
+            //trata do registro
+        }
+    }
+
+
+
 }
